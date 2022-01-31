7a76fc3185c4a4343ea74547fa4c1d442400f1e6
Franklin Yuan <yuan.franklin@gmail.com>
2022-01-30 22:05:20 -0700

    Update 4b.py

--- homework2/4b.py
+++ homework2/4b.py
@@ -32,81 +32,88 @@ class CipherProgram():
         arr.reverse()
         return arr
 
-    def encrypt(self, password, plaintext, file):
-        file.write(f"Encryption password: {password}\n\tPlaintext input: {plaintext}\n")
+    def encrypt(self, password, plaintext, output):
+        output.write(f"Encryption password: {password}\nPlaintext input: {plaintext}\n")
 
         # plaintext string -> int array
         int_array = list(map(ord, plaintext))
-        file.write(f"\tConvert to an int array: {list(map(hex, int_array))}\n")
+        output.write(f"\tConvert to an int array: {list(map(hex, int_array))}\n")
 
         # step 1. xor with password
         for i in range(8):
             int_array[i] = int_array[i] ^ ord(password[i])
-        file.write(f"\tXOR: {list(map(hex, int_array))}\n")
+        output.write(f"\tXOR: {list(map(hex, int_array))}\n")
         
         # step 2. character-by-character substitution
         for i in range(8):
             int_array[i] = self.sub_tables[i][0][int_array[i]]
-        file.write(f"\tSubstitution: {list(map(hex, int_array))}\n")
+        output.write(f"\tSubstitution: {list(map(hex, int_array))}\n")
 
         # step 3. permutation: 16 rounds of circular left shift
         int_64bit = self._intArrayTo64BitInt(int_array)
-        file.write(f"\tConvert to a 64-bit integer: {hex(int_64bit)}\n")
+        output.write(f"\tConvert to a 64-bit integer: {hex(int_64bit)}\n")
         for i in range(16):
             int_64bit = ((int_64bit << 1) & 0xffffffffffffffff) | (int_64bit >> 63)
-            file.write(f"\tPermutation round {i+1}: {hex(int_64bit)}\n")
+            output.write(f"\tPermutation round {i+1}: {hex(int_64bit)}\n")
         int_array = self._64BitIntToIntArray(int_64bit)
-        file.write(f"\tConvert back to an int array: {list(map(hex, int_array))}\n")
+        output.write(f"\tConvert back to an int array: {list(map(hex, int_array))}\n")
 
         # int array -> ciphertext string
         ciphertext = "".join(list(map(chr, int_array)))
-        file.write(f"\tCiphertext output: {ciphertext}\n\n")
+        output.write(f"Ciphertext output: {ciphertext}\n\n")
         return ciphertext
 
-    def decrypt(self, password, ciphertext, file):
-        file.write(f"Decryption password: {password}\n\tCiphertext input: {ciphertext}\n")
+    def decrypt(self, password, ciphertext, output):
+        output.write(f"Decryption password: {password}\nCiphertext input: {ciphertext}\n")
 
         # ciphertext string -> int array
         int_array = list(map(ord, ciphertext))
-        file.write(f"\tConvert to an int array: {list(map(hex, int_array))}\n")
+        output.write(f"\tConvert to an int array: {list(map(hex, int_array))}\n")
         
         # step 1. permutation: 16 rounds of circular right shift
         int_64bit = self._intArrayTo64BitInt(int_array)
-        file.write(f"\tConvert to a 64-bit integer: {hex(int_64bit)}\n")
+        output.write(f"\tConvert to a 64-bit integer: {hex(int_64bit)}\n")
         for i in range(16):
             int_64bit = (int_64bit >> 1) | ((int_64bit & 0x1) << 63)
-            file.write(f"\tPermutation round {i+1}: {hex(int_64bit)}\n")
+            output.write(f"\tPermutation round {i+1}: {hex(int_64bit)}\n")
         int_array = self._64BitIntToIntArray(int_64bit)
-        file.write(f"\tConvert back to an int array: {list(map(hex, int_array))}\n")
+        output.write(f"\tConvert back to an int array: {list(map(hex, int_array))}\n")
 
         # step 2. character-by-character substitution
         for i in range(8):
             int_array[i] = self.sub_tables[i][1][int_array[i]]
-        file.write(f"\tSubstitution: {list(map(hex, int_array))}\n")
+        output.write(f"\tSubstitution: {list(map(hex, int_array))}\n")
         
         # step 3. xor with password
         for i in range(8):
             int_array[i] = int_array[i] ^ ord(password[i])
-        file.write(f"\tXOR: {list(map(hex, int_array))}\n")
+        output.write(f"\tXOR: {list(map(hex, int_array))}\n")
         
         # int array -> plaintext string
         plaintext = "".join(list(map(chr, int_array)))
-        file.write(f"\tPlaintext output: {plaintext}\n\n")
+        output.write(f"Plaintext output: {plaintext}\n\n")
         return plaintext
+
+    # test whether decryption indeed reverses the encryption
+    def test(self, plaintext, password, output):
+        encrypted_text = self.encrypt(password, plaintext, output)
+        decrypted_text = self.decrypt(password, encrypted_text, output)
+        if decrypted_text == plaintext:
+            output.write("Decryption indeed reverses the encryption!\n\n")
+        else:
+            output.write("Decryption does not reverse the encryption!\n\n")
 
 def main():
     program = CipherProgram()
-    plaintext = "INeedAnA"
+    plaintext1 = "INeedAnA" # first input bit pattern
+    plaintext2 = "INeedAnC" # second input bit pattern, only one bit different from plaintext1
     password = "cs6490hw"
     # produce an encrypted output pattern
     with open("4b_output.txt", "w", encoding="utf-8") as f:
-        encrypted_text = program.encrypt(password, plaintext, f)
-        # feed the encrypted output pattern to the decyption function
-        decrypted_text = program.decrypt(password, encrypted_text, f)
-        if decrypted_text == plaintext:
-            f.write("Decryption indeed reverses the encryption!\n")
-        else:
-            f.write("Decryption does not reverse the encryption!\n")
+        f.write("Unit Test 1\n\n")
+        program.test(plaintext1, password, f)
+        f.write("Unit Test 2 (Changing only one bit in the input pattern)\n\n")
+        program.test(plaintext2, password, f)        
 
 if __name__ == "__main__":
     main()
