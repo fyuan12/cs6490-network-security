fc1ae56bbff5e3b0c3f40bda5bf998dcdbd59c82
Franklin Yuan <yuan.franklin@gmail.com>
2022-02-01 00:27:52 -0700

    Add homework 3

---
+++ homework3/README.md
@@ -0,0 +1,2 @@
+# CS 6490 Homework 3
+
---
+++ homework3/expo.py
@@ -0,0 +1,3 @@
+# Write a program to efficiently exponentiate big numbers modulo n
+# as described in Section 6.3.1
+# the program should take 3 positive numbers as input: m, d, and n
