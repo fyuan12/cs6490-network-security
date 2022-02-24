75da5b1451add1fd4fd3bb5f8425f79fd03528ca
Franklin Yuan <yuan.franklin@gmail.com>
2022-02-23 20:50:46 -0700

    experiment with crypto library

---
+++ pp1/alice.py
@@ -0,0 +1,18 @@
+import socket
+
+g, p = 1907, 784313
+sa = 160031
+PORT = 10000
+
+# This is client Alice
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect(('', PORT))
+    # g_sa = modular_expo(g, sa, p)
+    s.sendall(str(g_sa).encode('utf8'))
+    
+    data = s.recv(1024)
+    g_sb = int(str(data, 'utf8'))
+    # shared_key = modular_expo(g_sb, sa, p)
+
+    # print(f"Number sent by Alice: {g_sa}")
+    # print(f"Alice's shared key: {shared_key}")
---
+++ pp1/crypto.py
@@ -0,0 +1,40 @@
+from Crypto.Cipher import DES3
+from Crypto.Util.Padding import pad, unpad
+from Crypto.Random import get_random_bytes
+from base64 import b64encode, b64decode
+
+class EcbCipher:
+
+    def __init__(self, key):
+        self.cipher = DES3.new(key, DES3.MODE_ECB)
+    
+    def encrypt(self, plaintext):
+        return self.cipher.encrypt(pad(plaintext, DES3.block_size))
+
+    def decrypt(self, ciphertext):
+        return unpad(self.cipher.decrypt(ciphertext), DES3.block_size)
+
+class CbcCipher:
+
+    def __init__(self, key, iv=None):
+        if iv is None:
+            self.cipher = DES3.new(key, DES3.MODE_CBC)
+        else:
+            self.cipher = DES3.new(key, DES3.MODE_CBC, iv=iv)
+
+    def encrypt(self, plaintext):
+        return self.cipher.encrypt(pad(plaintext))
+
+    def decrypt(self, ciphertext):
+        return unpad(self.cipher.decrypt(ciphertext))
+
+def main():
+    data = b'secret'
+    key = get_random_bytes(16)
+    cipher = EcbCipher(key)
+    ciphertext = cipher.encrypt(data)
+    plaintext = cipher.decrypt(ciphertext)
+    print(plaintext)
+
+if __name__ == '__main__':
+    main()
